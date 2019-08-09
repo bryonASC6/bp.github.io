c671c985dbcf5b2072bfd44f346591c25cc7c9d3
bryonASC6 <bphil622@gmail.com>
2019-08-09 11:37:10 -0400

    added personal website files

---
+++ Bryon.js
@@ -0,0 +1,19 @@
+//get references
+let randomSite;
+let gamesArray;
+
+//declare an array (with different links in it)
+
+let gamesArray=["https://xenoblade.fandom.com/wiki/Xenoblade_Wiki","https://suda51.fandom.com/wiki/No_More_Heroes","https://hollowknight.fandom.com/wiki/Hollow_Knight_Wiki","https://okami.fandom.com/wiki/%C5%8Ckami","https://darksouls.fandom.com/wiki/Dark_Souls","https://crawl.fandom.com/wiki/Crawl_Wiki","https://www.smashbros.com/en_US/"];
+
+// generate a random #
+let randomSite= parseInt(Math.random()*gamesArray);
+Math.floor()*gamesArray;
+//use the random # + array to pick the random link
+
+for(i=0; i<gamesArray; i++) {
+    if (i<gamesArray) {
+    }
+
+    
+}
